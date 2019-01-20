b5bd9560b737575f4b5d1dda6d4f3de01340763d
ApacheThunder <spenserwhaley@gmail.com>
2019-01-20 15:57:20 -0600

    Initial Commit
    Initial Commit for DungeonSeedMod

---
+++ DungeonSeedMod/DungeonSeedMod.cs
@@ -0,0 +1,95 @@
+﻿using Dungeonator;
+using UnityEngine;
+
+namespace DungeonSeedMod {
+
+    public class DungeonSeedMod : ETGModule {
+
+        public override void Init() { }
+        public override void Exit() { }
+
+        public override void Start() {
+            ETGModConsole.Commands.AddGroup("dungeonseed", delegate (string[] e) {
+                ETGModConsole.Log("[Dungeon Seed]  The following options are available for setting Dungeon Seeds:");
+                ETGModConsole.Log("checkseed\nsetseed\nnewseed\nreset\n\nTo set a custom seed, use 'setseed SEED1 SEED2' where SEED1 is the main Dungeon seed between 1 and 1000000000 and\n SEED2 is the RandomIntForRun seed. Set Seed2 to a value of 0 to 1000.\nNote that setting SEED1 to zero will disable seeded runs.\nTo ensure consistant results, set your seed after selecting a character at the breach but before leaving the breach.\nSetting a seed while in the middle of a run or via quick restart is not recommended.\nTo start a new seed, simply use 'dungeonseed newseed' and write down the displayed seed if you want to share it!.\nUse 'dungeonseed checkseed' to check your current seed.\n\nTo turn off custom seed mode, use 'dungeonseed reset' or set SEED1 argument of setseed to zero.");
+            });
+            ETGModConsole.Commands.GetGroup("dungeonseed").AddUnit("checkseed", delegate (string[] e) { InitializeSeed(CheckSeed: true); });
+            ETGModConsole.Commands.GetGroup("dungeonseed").AddUnit("setseed", SetSeed);
+            ETGModConsole.Commands.GetGroup("dungeonseed").AddUnit("newseed", delegate (string[] e) {
+                InitializeSeed(BraveRandom.GenerationRandomRange(1, 1000000000), Random.Range(0, 1000), false);
+            });
+            ETGModConsole.Commands.GetGroup("dungeonseed").AddUnit("reset", delegate (string[] e) {
+                InitializeSeed(RandomIntForCurrentRun: Random.Range(0, 1000));
+                ETGModConsole.Log("Custom seeds has been disabled. Please start a new run to ensure changes take effect.");
+            });
+
+        }
+
+        private static bool SeedArgCount(string[] args, int min, int max)
+        {
+            if (args.Length >= min && args.Length <= max) return true;
+            if (min == max)
+            {
+                ETGModConsole.Log("Error: need exactly " + min + " argument(s)");
+            }
+            else
+            {
+                ETGModConsole.Log("Error: need between " + min + " and " + max + " argument(s)");
+            }
+            return false;
+        }
+
+        private void SetSeed(string[] args)
+        {
+            Dungeon dungeon = GameManager.Instance.Dungeon;
+
+            if (!SeedArgCount(args, 2, 2)) return;
+            if (int.Parse(args[0]) > 1000000000)
+            {
+                ETGModConsole.Log("Error: Requested seed exceeds normal maximum value of 1000000000");
+                return;
+            }
+
+            if (int.Parse(args[0]) > 1000)
+            {
+                ETGModConsole.Log("Error: Requested RandomIntForRun seed exceeds normal maximum value of 1000");
+                return;
+            }
+
+            if (int.Parse(args[0]) < 1) { ETGModConsole.Log("Warning: Setting a seed of 0 will disable seeded runs!"); }
+
+            if (dungeon.LevelOverrideType != GameManager.LevelOverrideState.FOYER)
+            {
+                ETGModConsole.Log("Warning: Setting a seed while not at the Breach can result in inconsistant results!");
+            }
+
+            InitializeSeed(int.Parse(args[0]), int.Parse(args[1]), false);
+        }
+
+        private void InitializeSeed(int DungeonSeed = 0, int RandomIntForCurrentRun = 0, bool CheckSeed = false)
+        {
+            if (CheckSeed)
+            {
+                ETGModConsole.Log("IsSeeded Status: " + GameManager.Instance.IsSeeded.ToString());
+                // ETGModConsole.Log("Current Dungeon Seed: " + GameManager.Instance.Dungeon.DungeonSeed);
+                ETGModConsole.Log("CurrentRunSeed: " + GameManager.Instance.CurrentRunSeed);
+                ETGModConsole.Log("RandomIntForRun: " + GameManager.Instance.RandomIntForCurrentRun);
+                ETGModConsole.Log("GetDungeonSeed Test: " + GameManager.Instance.Dungeon.GetDungeonSeed());
+                ETGModConsole.Log("If 'GetDungeonSeed Test' returns a random value on multiple checks, seeded runs is not active.");
+                return;
+            }
+            ETGModConsole.Log("Old CurrentRunSeed: " + GameManager.Instance.CurrentRunSeed);
+            ETGModConsole.Log("Old RandomIntForCurrentRun: " + GameManager.Instance.RandomIntForCurrentRun);
+            // ETGModConsole.Log("Old DungeonSeed: " + GameManager.Instance.Dungeon.DungeonSeed);
+            GameManager.Instance.Dungeon.DungeonSeed = DungeonSeed;
+            GameManager.Instance.CurrentRunSeed = DungeonSeed;
+            GameManager.Instance.RandomIntForCurrentRun = RandomIntForCurrentRun;
+            GameManager.Instance.Dungeon.GetDungeonSeed();
+            ETGModConsole.Log("New CurrentRunSeed: " + GameManager.Instance.CurrentRunSeed, false);
+            ETGModConsole.Log("New RandomIntForCurrentRun: " + GameManager.Instance.RandomIntForCurrentRun);
+            // ETGModConsole.Log("New Dungeon Seed: " + GameManager.Instance.Dungeon.DungeonSeed);
+            return;
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# DungeonSeedMod
-A mod allowing seeded runs in Enter the Gungeon!
+Dungeon Seed Mod - v 1.0 by Apache Thunder.
+
+Features:
+
+* Ability to set custom seeds for seeded runs! Note that certain player actions/choices on a run can impact certain elements of the run.
+But theoretically, if you made the same choices as the player who made the seed, you'd get the same results.
+
+Command syntax:
+
+To set a custom seed use "dungeonseed setseed SEED1 SEED2". SEED1 is the main Gungeon seed and expects a value between 1 and 1000000000.
+SEED2 is the RandomIntForCurrentRun seed. This one expects a value between 0 and 1000. Note that setting SEED1 to zero will disable seeded runs.
+
+For best results start a custom seed or new random seed from the Breach with a character already selected.
+
+To start a new seed, use "dungeonseed newseed" to generate a random seed to run on. Write down the seed you see in the console if you want to share it!
+
+To check the current seed you are using use "dungeonseed checkseed".
+To reset/disable seeded runs, use "dungeonseed reset".
+
+Compiling and versions of Enter the Gungeon required:
+
+This source code uses C#. Visual Studio 2015 or newer recommended.
+This mod is intended for post AG&D versions of Enter the Gungeon. Please ensure your game is up to date before attempting to use this mod.
